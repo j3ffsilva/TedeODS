1e4c561578bc003a7c5ae297cfdf15988101afeb
Jefferson O. Silva <silva.o.jefferson@gmail.com>
2025-02-28 21:23:29 -0300

    fix: implementar lookaheads positivos na string de busca ODS11

--- classifiers/ods11_classifier.py
+++ classifiers/ods11_classifier.py
@@ -6,11 +6,8 @@ class ODS11Classifier(BaseODSClassifier):
 
     def get_padroes(self):
         return [
-            # Grupo 1: Termos relacionados a "cidade"
-            r'\b(?:cidade[s]?|assentamento[s]?\s*humano[s]?|urbano|metr[óo]pol\w*|vila\w*|municipal\w*)\b'
-            r'.*'
-            # Grupo 2: Termos relacionados a questões urbanas
-            r'\b(?:gentrifica[çc][ãa]o|congestionamento|transporte(?:\s*p[úu]blico)?|moradia|favela\w*|'
+            r'(?=.*\b(?:cidade[s]?|assentamento[s]?\s*humano[s]?|urbano|metr[óo]pol\w*|vila\w*|municipal\w*)\b)'
+            r'(?=.*\b(?:gentrifica[çc][ãa]o|congestionamento|transporte(?:\s*p[úu]blico)?|moradia|favela\w*|'
             r'sendai\s*framework|redu[çc][ãa]o\s*de\s*risco\s*de\s*desastres|rrd|'
             r'cidade[s]?\s*inteligente[s]?|edif[íi]cio[s]?\s*(?:resiliente[s]?|sustent[áa]vel[eis]?)|'
             r'design\s*de\s*edif[íi]cios|projeto\s*de\s*edif[íi]cios|urbaniza[çc][ãa]o|'
@@ -20,7 +17,7 @@ class ODS11Classifier(BaseODSClassifier):
             r'res[íi]duos|polui[çc][ãa]o|poluente\w*|[áa]gua\s*residual|reciclagem|'
             r'economia\s*circular|qualidade\s*do\s*ar|espa[çc]o[s]?\s*verde[s]?|'
             r'que\s*inclui\s*a\s*natureza|(?:edif[íi]cio[s]?|pr[ée]dio[s]?)\s*que\s*inclui\s*a\s*natureza|'
-            r'(?:edif[íi]cio[s]?|pr[ée]dio[s]?)\s*inclusivo[s]?\s*da\s*natureza)\b'
+            r'(?:edif[íi]cio[s]?|pr[ée]dio[s]?)\s*inclusivo[s]?\s*da\s*natureza)\b)'
         ]
 
     def get_excecoes(self):
